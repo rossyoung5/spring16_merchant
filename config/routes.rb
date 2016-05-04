1c9b6182c277022281ce1a063ebcd4d07cfffee6
Ross Young <ross.young12@gmail.com>
2016-05-04 09:11:21 -0500

    moves folders to admin folders and appends controller classes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :products
-  resources :brands
-  resources :categories
+  namespace :admin do
+    resources :products
+    resources :brands
+    resources :categories
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
