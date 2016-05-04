3f72b976b299d771896cf316fb27e8d51a222096
Ross Young <ross.young12@gmail.com>
2016-05-04 08:51:54 -0500

    adds product scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :products
   resources :brands
   resources :categories
   # The priority is based upon order of creation: first created -> highest priority.
