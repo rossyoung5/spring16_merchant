6456a1babfe7d0564a8bfb30f8aae931aac16ee8
Ross Young <ross.young12@gmail.com>
2016-05-05 08:47:23 -0500

    adds thumbnail to table and fixes names for brands and categories

--- app/models/product.rb
+++ app/models/product.rb
@@ -5,7 +5,7 @@ class Product < ActiveRecord::Base
   has_attached_file :avatar,
     :styles => {
       :medium => "400x400#",
-      :thumb => "150x150#"
+      :thumb => "100x100#"
       },
       :default_url => "images/:style/missing.png"
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
