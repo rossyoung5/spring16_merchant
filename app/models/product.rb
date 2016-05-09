1ffcf570fec857b376d8de5e17ff48851b1f4e6f
Ross Young <ross.young12@gmail.com>
2016-05-09 09:09:46 -0500

    adds storefront controller and updates index view with bootstrap modals

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,7 +4,7 @@ class Product < ActiveRecord::Base
 
   has_attached_file :avatar,
     :styles => {
-      :medium => "400x400#",
+      :medium => "300x300#",
       :thumb => "100x100#"
       },
       :default_url => "missing_:style.png"
