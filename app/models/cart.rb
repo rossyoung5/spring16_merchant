9270cf1c204c29a25716d01b665f8d45b0e11052
Ross Young <ross.young12@gmail.com>
2016-05-10 09:55:54 -0500

    sets destroy method for cart

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,17 @@
 class Cart < ActiveRecord::Base
   has_many :line_items, dependent: :destroy
 
+  def destroy
+    if @cart.destroy
+      session[:cart_id] = nil
+      flash[:notice] = "Cart was successfully deleted."
+      redirect_to shop_path
+    else
+      flash.now[:alert] = "Could not delete cart."
+      redirect_to edit_cart_path
+    end
+  end
+
   def subtotal
     #SQL version of summing, which is faster than Ruby
     line_items.select("SUM(quantity * price) AS sum")[0].sum
